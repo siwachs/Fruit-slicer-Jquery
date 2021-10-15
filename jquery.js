5af3b829efeb716109846f5326917fb37a8d19e2
ssiwach8888 <ssi.xxxx888@gmail.com>
2021-10-15 12:17:18 +0530

    second

--- jquery.js
+++ jquery.js
@@ -1,25 +1,18 @@
 var is_paly=false;
 var score;
-$(function()
-{
-    $("#bt").click(
-        function()
-        {
-            if(is_paly)
-            {
-                is_paly=false;
-                location.reload();
-            }
-            else
-            {
-                is_paly=true;
-                score=0;
-                $("#bt").text("Reset");
-                //$("#score_var").text(score);
-                $("#score_val").html(score);
-                //use show or hide method
-                
-            }
+
+$(function(){
+    //jquery click is depreted we can use on method to setup a event handler
+    $('#bt').on('click',function(){
+        if(is_paly){
+            location.reload();
+        }else{
+            is_paly=true;
+            $('#bt').text('Reset');
+            score=0;
+            $('score_val').text(score);
+            //use hide and show
+            $('#life').show();
         }
-    );
+    });
 });
